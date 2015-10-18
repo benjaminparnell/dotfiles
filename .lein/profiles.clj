4e04d7dd600398cfaa9ea82abf62f22cca6403c8
Ben Parnell <benjaminparnell.94@gmail.com>
2015-10-18 16:11:39 +0100

    add some leiningen plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,9 @@
 {:user {:plugins [[lein-exec "0.3.5"]
-                  [cider/cider-nrepl "0.9.1"]]}}
+                  [cider/cider-nrepl "0.9.1"]
+                  [jonase/eastwood "0.2.1"]
+                  [lein-kibit "0.1.2"]
+                  [lein-pprint "1.1.1"]
+                  [lein-marginalia "0.8.0"]
+                  [lein-licenses "0.2.0"]]
+        :dependencies [[slamhound "1.5.5"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
